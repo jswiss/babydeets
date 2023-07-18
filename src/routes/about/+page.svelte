ccdd82186b0c1650aac4bf83571e43b6a5fa00a8
jswiss <swissjoshua@gmail.com>
2023-07-18 13:19:05 +0100

    initial commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About this app</h1>
+
+	<p>This is a desktop app to keep track of notes about your baby.</p>
+
+	<p>
+		Go to <a href="/add-deets">Add Deets</a> to start adding notes about your baby!
+	</p>
+</div>
---
+++ src/routes/add-deets/+page.svelte
@@ -0,0 +1,34 @@
+<svelte:head>
+	<title>Add Baby Details</title>
+	<meta name="Add Baby Details" content="Use this form to add details about your baby" />
+</svelte:head>
+
+<section>
+	<h1>Add Deets</h1>
+
+	<form method="POST">
+		<label>
+			Date
+			<input name="date" type="date" />
+		</label>
+		<label>
+			Notes
+			<input name="notes" type="textarea" />
+		</label>
+		<button>Submit</button>
+	</form>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
