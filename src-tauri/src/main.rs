60ef0927eac580aaa23b4bdcaee062612a40ef52
jswiss <swissjoshua@gmail.com>
2023-08-21 16:13:45 +0100

    Add colour eyre

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::env;
+use color_eyre::eyre::Result;
+
 mod models;
 mod schema;
 mod db;
@@ -11,15 +13,21 @@ mod services;
 use crate::db::db_init;
 use crate::commands::baby_commands::*;
 
-fn main() {
+fn main() -> Result<()> {
+  color_eyre::install()?;
+
   tauri::Builder::default()
     .setup(|_app| {
       db_init();
       Ok(())
     })
     .invoke_handler(tauri::generate_handler![
-      create_baby
+      create_baby,
+      list_babies
     ])
+    .plugin(tauri_plugin_sqlite_store::init())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+
+    Ok(())
 }
