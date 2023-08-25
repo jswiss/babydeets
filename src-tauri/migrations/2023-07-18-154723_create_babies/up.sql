456a38f51508e3a7f1b8e5b139968e3bd41aeb53
jswiss <swissjoshua@gmail.com>
2023-08-25 15:42:44 +0100

    update models

--- src-tauri/migrations/2023-07-18-154723_create_babies/up.sql
+++ src-tauri/migrations/2023-07-18-154723_create_babies/up.sql
@@ -2,7 +2,9 @@
 CREATE TABLE babies (
   id VARCHAR(50) PRIMARY KEY NOT NULL,
   name VARCHAR(100) NOT NULL,
+  photo BLOB,
   sex VARCHAR(10) NOT NULL,
   birthday TEXT NOT NULL,
-  created_at TEXT NOT NULL
+  created_at TEXT NOT NULL,
+  updated_at TEXT NOT NULL
 )
--- src-tauri/src/models/baby.rs
+++ src-tauri/src/models/baby.rs
@@ -2,20 +2,15 @@ use diesel::prelude::*;
 use diesel::{Insertable, Queryable};
 use serde::Serialize;
 
-#[derive(Queryable, Serialize, Selectable, Insertable)]
+#[derive(Queryable, Serialize, serde::Deserialize, Selectable, Insertable)]
 #[diesel(table_name = crate::schema::babies)]
 #[derive(Debug)]
 pub struct Baby {
   pub id: String,
   pub name: String,
+  pub photo: Option<Vec<u8>>,
   pub sex: String,
   pub birthday: String,
-  pub created_at: String
-}
-
-#[derive(serde::Deserialize)]
-pub struct NewBaby {
-  pub name: String,
-  pub sex: String,
-  pub birthday: String
+  pub created_at: String,
+  pub updated_at: String
 }
