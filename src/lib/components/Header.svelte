3de5a86d255b0337f1b643f28c174413f43de8ed
jswiss <swissjoshua@gmail.com>
2023-08-25 16:57:35 +0100

    tidy up default files

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -25,9 +25,6 @@
 			<li aria-current={$page.url.pathname === '/add-deets' ? 'page' : undefined}>
 				<a href="/add-deets">Add Deets</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
@@ -35,7 +32,7 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
+		<a href="https://github.com/jswiss/babydeets">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
