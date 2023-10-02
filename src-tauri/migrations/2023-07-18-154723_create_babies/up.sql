a7208d075e7b42554cf68bce0a899fdd96e8580b
jswiss <swissjoshua@gmail.com>
2023-10-02 14:09:42 +0100

    switch img and files to paths

--- src-tauri/migrations/2023-07-18-154723_create_babies/up.sql
+++ src-tauri/migrations/2023-07-18-154723_create_babies/up.sql
@@ -2,7 +2,7 @@
 CREATE TABLE babies (
   id VARCHAR(50) PRIMARY KEY NOT NULL,
   name VARCHAR(100) NOT NULL,
-  photo BLOB,
+  photo VARCHAR(150),
   sex VARCHAR(10) NOT NULL,
   birthday TEXT NOT NULL,
   created_at TEXT NOT NULL,
